e7cc2b1463c207f7b8a0a96f80141e270a7254d6
Victor Cha <112844634+OzarkCrypto@users.noreply.github.com>
2026-03-27 13:22:52 +0800

    Update RootLayout to remove font links and change background
    Removed font preconnect links and changed body background color to white.

--- app/layout.js
+++ app/layout.js
@@ -6,15 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700&family=Outfit:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body style={{ margin: 0, background: "#050505" }}>{children}</body>
+      <body style={{ margin: 0, background: "#fff" }}>{children}</body>
     </html>
   );
 }
